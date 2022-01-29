2241356192db4425dd3d81c12e96caafaa514af1
Shadow15510 <antoine.royer14@orange.fr>
2022-01-29 10:54:42 +0100

    Update automap.sh

--- tiled_map/automap.sh
+++ tiled_map/automap.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+mkdir map_python
+
 for i in $(ls -X *.tmx)
 do
-	echo "$i"
+	echo "$i -> ${i%%.*}.py"
 	python converter "$i" doors=^ entities=?*
+	cp "${i%%.*}.py" map_python/
+	rm "${i%%.*}.py"
 done
+
